6227381d437c5f118f2f5b3a5f756f4393e073d8
Ivanoka <ivantitov2002@mail.ru>
2022-02-22 21:37:02 +0300

    inicial commit

--- dutymodule.py
+++ dutymodule.py
@@ -5,24 +5,25 @@ import json
 import sqlite3
 import lib.vk_api_functions as vk_api_functions
 
+sqlConnection = sqlite3.connect('.\db\database.db')
+sqlCursor = sqlConnection.cursor()
 
-sql_connection = sqlite3.connect('.\db\database.db')
-sql_cursor = sql_connection.cursor()
-sql_cursor.execute('SELECT name from sqlite_master where type = "table"')
-print(sql_cursor.fetchall()[0][0])
 
 
 def SendNotification():
-    with open('.\config\duty_notification_setings.json', 'r', encoding='utf-8') as json_cfg:
-        config = json.load(json_cfg)
-    if config["5142211"][str((date.today()).weekday())]["SENDFLAG"]:
-        if config["5142211"][str((date.today()).weekday())]["TIMESEND"] == (datetime.now()).strftime('%H:%M'):
-            vk_api_functions.MessangeSend.Chat(2000000002, "СЕГОДНЯ СУЧКИ МЫ БУДЕМ ЕБАТЬСЯ ДОЛГО И ПЛОТНО")
-            time.sleep(10)
+    sqlCursor.execute("SELECT * FROM GROUPS WHERE DUTYENABLE = 1")
+    groups = sqlCursor.fetchall()
+    for numberGroup in groups:
+        with open('.\config\duty_notification_setings.json', 'r', encoding='utf-8') as duNotSend:
+            config = json.load(duNotSend)
+        if config[str(numberGroup[0])][str((date.today()).weekday())]["SENDFLAG"]:
+            if config[str(numberGroup[0])][str((date.today()).weekday())]["TIMESEND"] == (datetime.now()).strftime('%H:%M'):
+                vk_api_functions.MessangeSend.Chat(2, "СЕГОДНЯ СУЧКИ МЫ БУДЕМ ЕБАТЬСЯ ДОЛГО И ПЛОТНО")
+                time.sleep(10)
+            else:
+                time.sleep(10)
         else:
             time.sleep(10)
-    else:
-        time.sleep(10)
 
 
 
