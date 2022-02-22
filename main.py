6296bfd944f658e63032728e4552ec9e9b4277f6
isnullibi <isnullibi@gmail.com>
2022-02-22 22:49:32 +0300

    Update main.py
    Added new command & buttons

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os, telebot, time
 from dotenv import load_dotenv
 from datetime import datetime
 from pytz import timezone
+from telebot import types
 
 DATE = datetime.now(timezone('Europe/Moscow'))
 
@@ -32,14 +33,34 @@ def main():
                                               f".{DATE.month}"
                                               f".{DATE.year}")
 
+    # ВЫВОД СООБЩЕНИЯ ПРИ КОМАНДЕ START
+    @bot.message_handler(commands='start')
+    def start_message(start_msg):
+        keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        slave_button = types.KeyboardButton(text="Узнать имя работяги")
+        keyboard.add(slave_button)
+        next_slave_button = types.KeyboardButton(text="Кто же следующий")
+        keyboard.add(next_slave_button)
+        help_button = types.KeyboardButton(text="/help")
+        keyboard.add(help_button)
+        bot.send_message(start_msg.chat.id, "Для получения списка комманд введи /help", reply_markup=keyboard)
+
+    # ПРОСЛУШКА ЧАТА И РЕАКЦИИ
     @bot.message_handler(content_types=['text'])
     def get_text_message(message):
-        if message.text == "Привет":
+        if message.text == "Привет" or message.text == "/start":
             bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
         elif message.text == "/help":
-            bot.send_message(message.from_user.id, "Напиши 'Привет'")
+            bot.send_message(message.from_user.id, "/worker - Узнать, кто сегодня дежурит\n"
+                                                   "/nextworker - Узнать, кто дежурит следующим")
+        elif message.text == "/worker" or message.text == "Узнать имя работяги":
+            bot.send_message(message.from_user.id, "Worker WIP")
+            #TODO func worker
+        elif message.text == "/nextworker" or message.text == "Кто же следующий":
+            bot.send_message(message.from_user.id, "NextWorker WIP")
+            #TODO func nextworker
         else:
-            bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
+            bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help или используй кнопку")
 
     bot.polling(none_stop=True, interval=0)
 
