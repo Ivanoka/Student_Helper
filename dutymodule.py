5c0bc41ae12f6381d35081ad794849dcc27afafe
Ivanoka <ivantitov2002@mail.ru>
2022-02-22 22:53:15 +0300

    inicial commit

--- dutymodule.py
+++ dutymodule.py
@@ -11,19 +11,24 @@ sqlCursor = sqlConnection.cursor()
 
 
 def SendNotification():
+    with open('.\config\duty_notification_setings.json', 'r', encoding='utf-8') as dutyNotifSend:
+                config = json.load(dutyNotifSend)
     sqlCursor.execute("SELECT * FROM GROUPS WHERE DUTYENABLE = 1")
     groups = sqlCursor.fetchall()
-    for numberGroup in groups:
-        with open('.\config\duty_notification_setings.json', 'r', encoding='utf-8') as duNotSend:
-            config = json.load(duNotSend)
-        if config[str(numberGroup[0])][str((date.today()).weekday())]["SENDFLAG"]:
-            if config[str(numberGroup[0])][str((date.today()).weekday())]["TIMESEND"] == (datetime.now()).strftime('%H:%M'):
-                vk_api_functions.MessangeSend.Chat(2, "СЕГОДНЯ СУЧКИ МЫ БУДЕМ ЕБАТЬСЯ ДОЛГО И ПЛОТНО")
-                time.sleep(10)
+    if groups == None:
+        time.sleep(10)
+    else:
+        for numberGroup in groups:
+            with open('.\config\duty_notification_setings.json', 'r', encoding='utf-8') as dutyNotifSend:
+                config = json.load(dutyNotifSend)
+            if config[str(numberGroup[0])][str((date.today()).weekday())]["SENDFLAG"]:
+                if config[str(numberGroup[0])][str((date.today()).weekday())]["TIMESEND"] == (datetime.now()).strftime('%H:%M'):
+                    vk_api_functions.MessangeSend.Chat(2, "СЕГОДНЯ СУЧКИ МЫ БУДЕМ ЕБАТЬСЯ ДОЛГО И ПЛОТНО")
+                    time.sleep(10)
+                else:
+                    time.sleep(10)
             else:
                 time.sleep(10)
-        else:
-            time.sleep(10)
 
 
 
