9a4dff3cf4b8c3bfda7ff56e3a6ed035d80917cd
Ivanoka <ivantitov2002@mail.ru>
2022-02-23 23:59:22 +0300

    inicial commit

--- dutymodule.py
+++ dutymodule.py
@@ -3,43 +3,55 @@ from datetime import datetime
 from datetime import date
 import json
 import sqlite3
-import lib.vk_api_functions as vk_api_functions
+import lib.vk_api_functions as VkApiFunctions
 
-sqlConnection = sqlite3.connect('.\db\database.db')
+sqlConnection = sqlite3.connect('db\database.db')
 sqlCursor = sqlConnection.cursor()
 
 
 
-def SelectionDuty():
-    pass
-
-def SendNotification():
-    with open('.\config\duty_notification_setings.json', 'r', encoding='utf-8') as dutyNotifSend:
-                config = json.load(dutyNotifSend)
-    sqlCursor.execute("SELECT GROUPNUMBER FROM GROUPS WHERE DUTYENABLE = 1")
-    groups = sqlCursor.fetchall()
-    if groups == None:
-        time.sleep(10)
-    else:
-        for numberGroup in groups:
-            with open('.\config\duty_notification_setings.json', 'r', encoding='utf-8') as dutyNotifSend:
-                config = json.load(dutyNotifSend)
-            if config[str(numberGroup)][str((date.today()).weekday())]["SENDFLAG"]:
-                if config[str(numberGroup)][str((date.today()).weekday())]["TIMESEND"] == (datetime.now()).strftime('%H:%M'):
-                    sqlCursor.execute("SELECT ID FROM " + str(numberGroup) + " WHERE LASTONDUTY = 1")
-                    idDuty = sqlCursor.fetchone()
-                    if idDuty == None:
-                        sqlCursor.execute("SELECT COUNT(*) FROM " + numberGroup)
-                        countId = sqlCursor.fetchall()
-                        sqlCursor.execute("UPDATE " + numberGroup + "LASTONDUTY = 1 WHERE ID = " + (date.today()).day % countId)
-                        vk_api_functions.MessangeSend.Chat
-                else:
-                    time.sleep(10)
-            else:
-                time.sleep(10)
-
+def SelectionNewDuty(numberGroup):
+    flag = True
+    
+    sqlCursor.execute("SELECT COUNT(*) FROM '" + str(numberGroup) + "'")
+    countId = sqlCursor.fetchall()
+    countId = countId[0][0]
+    newDuty = (date.today()).day % countId
+    print(newDuty)
+    while flag:
+        sqlCursor.execute("SELECT SICK FROM  '" + str(numberGroup) + "' WHERE ID = " + str(newDuty))
+        newDutySick = sqlCursor.fetchone()
+        newDutySick = newDutySick[0]
+        if newDutySick == 0:
+            sqlCursor.execute("UPDATE '" + str(numberGroup) + "' SET LASTONDUTY = 1 WHERE ID = " + str(newDuty))
+            sqlConnection.commit
+            print("Дежурный выбран")
+            flag = False
 
 
 def DutyModuleMain():
     while True:
-        SendNotification()
+        print(date.today().weekday())
+        with open('.\config\duty_notification_setings.json', 'r', encoding='utf-8') as dutyNotifSend:
+                config = json.load(dutyNotifSend)
+        sqlCursor.execute("SELECT GROUPNUMBER FROM GROUPS WHERE DUTYENABLE = 1")
+        groups = sqlCursor.fetchall()
+        if groups == None:
+            time.sleep(10)
+        else:
+            for numberGroup in groups:
+                with open('.\config\duty_notification_setings.json', 'r', encoding='utf-8') as dutyNotifSend:
+                    config = json.load(dutyNotifSend)
+                if config[str(numberGroup[0])][str(date.today().weekday())]["SENDFLAG"]:
+                    if config[str(numberGroup[0])][str(date.today().weekday())]["TIMESEND"] == (datetime.now()).strftime('%H:%M'):
+                        sqlCursor.execute("SELECT ID FROM '" + str(numberGroup[0]) + "' WHERE LASTONDUTY = 1")
+                        idDuty = sqlCursor.fetchone()
+                        if idDuty == None:
+                            SelectionNewDuty(numberGroup[0])
+                            time.sleep(10)
+                        else:
+                            time.sleep(10)     
+                    else:
+                        time.sleep(10)
+                else:
+                    time.sleep(10)
--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 import asyncio
+import sqlite3
 from dutymodule import DutyModuleMain
 
 
+"""if __name__ == "__main__":
+    asyncio.run(DutyModuleMain())"""
+    
+sqlConnection = sqlite3.connect('db\database.db')
+sqlCursor = sqlConnection.cursor()
 
-if __name__ == "__main__":
-    asyncio.run(DutyModuleMain())
+sqlCursor.execute("UPDATE '5142211' SET LASTONDUTY = 1 WHERE ID = 2")
+sqlConnection.commit
+sqlCursor.close()
