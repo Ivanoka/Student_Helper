8a89727f4306157251471a125c48de1d4cf2e5da
Ivanoka <ivantitov2002@mail.ru>
2022-02-25 00:07:34 +0300

    inicial commit

--- chatRegistration.py
+++ chatRegistration.py
@@ -1,31 +1,25 @@
 import lib.vk_api_functions as VkApiFuntions
 import sqlite3
+import sys
 
-
+peerId = int(sys.argv[1])
 sqlConnection = sqlite3.connect('db\database.db')
 sqlCursor = sqlConnection.cursor()
 
 
 
-while True:
+VkApiFuntions.MessangeSend.Chat(peerId - 2000000000, "Вы подключили Помошника студента в вашу бесседу.\nДля регистрации назначьте нас Администратором и напишите номер группы\nДля регестрации у вас есть 5 минут.")
+sqlCursor.execute("SELECT GROUPNUMBER FROM GROUPS WHERE VKID IS NULL")
+groupsNumder = sqlCursor.fetchone()
+flag = True
+while flag:
     event = VkApiFuntions.LongPollListen()
-    try:
-        if event.message.action["type"] == "chat_invite_user":
-            VkApiFuntions.MessangeSend.Chat(event.message.peer_id - 2000000000, "Чтобы начать назначьте Бота администратором беседы и напишите номер группы.")
-            sqlCursor.execute("SELECT GROUPNUMBER FROM GROUPS WHERE VKID IS NULL")
-            groupsNumder = sqlCursor.fetchone()
-            peerId = event.message.peer_id
-            flag = True
-            while flag:
-                event = VkApiFuntions.LongPollListen()
-                if event.message.peer_id == peerId:
-                    checkFlag = True
-                    for number in groupsNumder:
-                        print(number)
-                        if event.message.text == str(number):
-                            VkApiFuntions.MessangeSend.Chat(peerId - 2000000000, "Группа " + str(number) + " зарегистрирована")
-                            checkFlag, flag = False, False
-                    if checkFlag: 
-                        VkApiFuntions.MessangeSend.Chat(peerId - 2000000000, "Номер группы не найден, попробуйте заново.")
-    except:
-        pass
+    if event.message.peer_id == peerId:
+        checkFlag = True
+        for number in groupsNumder:
+            print(number)
+            if event.message.text == str(number):
+                VkApiFuntions.MessangeSend.Chat(peerId - 2000000000, f"Группа {number} зарегистрирована")
+                checkFlag, flag = False, False
+        if checkFlag: 
+            VkApiFuntions.MessangeSend.Chat(peerId - 2000000000, "Номер группы не найден, попробуйте заново.")
--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 import sys
 import subprocess
+import lib.vk_api_functions as VkApiFuntions
 
 
 subprocess.Popen([sys.executable, 'dutyModule.py'])
-subprocess.Popen([sys.executable, 'chatRegistration.py'])
+
+while True:
+    event = VkApiFuntions.LongPollListen()
+    try:
+        if event.message.action["type"] == "chat_invite_user":
+            subprocess.Popen([sys.executable, 'chatRegistration.py', str(event.message.peer_id)])
+    except:
+        pass
